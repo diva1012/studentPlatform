ae4afb91653b51b7a058180aa7cdd19e73f134aa
Vassil Dimov <v.dimov@campus.tu-berlin.de>
2015-12-16 15:02:23 +0100

    Facebook integration prepared

---
+++ client/facebook/facebook.js
@@ -0,0 +1,50 @@
+Template.Facebook.rendered = function() {
+  Meteor.call('getUserData', function(error, respJson) {
+
+    if(error) {
+      console.log("error occured on receiving data from server. ", err );
+    } else {
+      Session.set("userData", respJson.data);
+    }
+  });
+}
+
+Template.Facebook.helpers({
+  userData: function(){
+    return Session.get("userData");
+  }
+});
+
+
+Template.Facebook.events({
+    'click #btn-user-data': function(e) {
+        Meteor.call('getUserData', function(err, data) {
+             $('#result').text(JSON.stringify(data, undefined, 4));
+         });
+    },
+
+    'click #btn-user-friendsdata': function(e) {
+            Meteor.call('getFriendsData', function(err, data) {
+                 $('#result-friends').text(JSON.stringify(data, undefined, 4));
+             });
+    },
+
+  'click #btn-user-groups': function(e) {
+        Meteor.call('getGroupsData', function(err, data) {
+             $('#result-groups').text(JSON.stringify(data, undefined, 4));
+         });
+    },
+
+  'click #btn-user-files': function(e) {
+        Meteor.call('getGroupsFiles', function(err, data) {
+             $('#result-files').text(JSON.stringify(data, undefined, 4));
+         });
+    },
+
+    'click #btn-user-post': function(e) {
+        Meteor.call('getGroupsPost', function(err, data) {
+             $('#result-post').text(JSON.stringify(data, undefined, 4));
+         });
+    },
+
+});
--- server/methods/facebook.js
+++ server/methods/facebook.js
@@ -1,6 +1,18 @@
 function Facebook(accessToken) {
     this.fb = Meteor.npmRequire('fbgraph');
+
+    // START Daten
+    var myTocken= "185013235166746|pz5rzkCJAD_jkZfW-o_oMyJXQqM";
+    var groupId
+    var accToken = {
+      "client_id": "185013235166746",
+      "client_secret": "a20027b9f642e601418bd6b63e22b0e6"
+    }
+    // END Daten
+
+    //this.accessToken = accToken;
     this.accessToken = accessToken;
+
     this.fb.setAccessToken(this.accessToken);
     this.options = {
         timeout: 3000,
@@ -18,19 +30,32 @@ Facebook.prototype.getFriendsData = function() {
     return this.query('/me/friendlists');
 }
 
-Meteor.methods({
-    getUserData: function() {
-        var fb = new Facebook(Meteor.user().services.facebook.accessToken);
-        var data = fb.getUserData();
-        return data;
-    },
+/*
+Facebook.prototype.getGroupsData = function() {
+    return this.query('/me/groups');
+}
+*/
 
-    getUserData: function() {
-        var fb = new Facebook(Meteor.user().services.facebook.accessToken);
-        var data = fb.getFriendsData();
-        return data;
-    }
-});
+Facebook.prototype.getGroupsData = function() {
+
+    //return this.query('/1714786542089655/feed');
+
+    return this.query('/178638415821259/feed?fields=picture,comments.limit(3),link');
+}
+
+Facebook.prototype.getGroupsFiles = function() {
+
+    //return this.query('/1714786542089655/feed');
+
+    return this.query('/178638415821259/files');
+}
+
+Facebook.prototype.getGroupsPost = function() {
+
+    //return this.query('/1714786542089655/feed');
+
+    return this.query('/178638415821259_178651539153280?fields=message,picture');
+}
 
 Facebook.prototype.query = function(query, method) {
     var self = this;
@@ -42,3 +67,39 @@ Facebook.prototype.query = function(query, method) {
     });
     return data.result;
 }
+
+
+Meteor.methods({
+    getUserData: function() {
+        var fb = new Facebook(Meteor.user().services.facebook.accessToken);
+        var data = fb.getUserData();
+        return data;
+    },
+
+    getFriendsData: function() {
+        var fb = new Facebook(Meteor.user().services.facebook.accessToken);
+        var data = fb.getFriendsData();
+        return data;
+    },
+
+    getGroupsData: function() {
+        var fb = new Facebook(Meteor.user().services.facebook.accessToken);
+        var data = fb.getGroupsData();
+        return data;
+    },
+
+    getGroupsFiles: function() {
+        var fb = new Facebook(Meteor.user().services.facebook.accessToken);
+        var data = fb.getGroupsFiles();
+        return data;
+    },
+
+    getGroupsPost: function() {
+        var fb = new Facebook(Meteor.user().services.facebook.accessToken);
+        var data = fb.getGroupsPost();
+        return data;
+    },
+
+
+
+});
